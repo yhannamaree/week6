410595c1f0e7382ebb17b076443e8fb2664d76a9
yhannamaree <111513727+yhannamaree@users.noreply.github.com>
2022-09-19 11:02:15 +0800

    Create App.java

---
+++ switch/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String today = "monday";
+
+        switch (today) {
+            case "monday":
+                System.out.println("I hate mondays!");
+                break;
+            case "tuesday":
+                System.out.println("My favorite day is tuesday!");
+                break;
+            case "wednesday":
+                System.out.println("I play cod mobile every wednesday!");
+                break;
+            case "thursday":
+                System.out.println("My stress day is on thursday!");
+                break;
+            case "friday":
+                System.out.println("I go on a online class during fridays!");
+                break;
+            case "Saturday":
+                System.out.println("I go to the SM every saturday!");
+                break;
+            case "Sunday":
+                System.out.println("I go to church every sunday!");
+                break;
+            default:
+                break;
+        }
+
+    }
+}
