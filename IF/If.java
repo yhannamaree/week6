76c3801f0c7c9ee5169c3afe98bd0fa8864c7116
yhannamaree <111513727+yhannamaree@users.noreply.github.com>
2022-09-19 11:44:19 +0800

    Create If.java

---
+++ IF/If.java
@@ -0,0 +1,50 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Conditional Structure
+        boolean ihaveNoUniform = true;
+        boolean ihaveNoFood = false;
+        boolean ihaveNoUmbrella = true;
+
+        if (ihaveNoUniform) {
+            System.out.println("Mag palaba");
+        } 
+        if (ihaveNoUniform) {
+            System.out.println("Mag padeliver");
+            
+            if (ihaveNoUniform) {
+            System.out.println("Mag Bili");
+        }
+        }
+        else {
+            // Step 1
+            walkMontana();
+
+            // Step 2
+            eatinkarinderya();
+
+            // Step 3
+            walktoNU();
+
+           
+        }
+    }
+
+    static void walkMontana() {
+        System.out.println("Walk in Montana");
+        System.out.println("Exit along Montana");
+    }
+
+    static void eatinkarinderya() {
+        System.out.println("Eat in Karinderya");
+        System.out.println("Exit at the Karinderya");
+    }
+
+    static void walktoNU() {
+        System.out.println("WalktoNU");
+        System.out.println("Exit at NU");
+    }
+
+  
+    
+}
